bcfa9cee24c3c267d103cd3da71b45d2659b7caa
AL HOUCINE  MAZZINE <hossinemazzine@gmail.com>
2025-12-14 00:20:49 +0100

    Delete react-todo/src/TodoList.test.js

--- react-todo/src/TodoList.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import TodoList from "./TodoList";
-
-test("renders TodoList component", () => {
-  render(<TodoList />);
-  expect(screen.getByText(/todo/i)).toBeInTheDocument();
-});
-
