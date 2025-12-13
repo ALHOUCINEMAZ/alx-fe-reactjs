6ae6370e0502d381a2510d9a657c17af4d6139cb
AL HOUCINE  MAZZINE <hossinemazzine@gmail.com>
2025-12-14 00:14:22 +0100

    Add test for rendering TodoList component

--- react-todo/src/TodoList.test.js
+++ react-todo/src/TodoList.test.js
@@ -1,1 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import TodoList from "./TodoList";
+
+test("renders TodoList component", () => {
+  render(<TodoList />);
+  expect(screen.getByText(/todo/i)).toBeInTheDocument();
+});
 
