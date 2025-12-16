09a3a72a0c0f97a684584d9e54c9cbd375432416
AL HOUCINE  MAZZINE <hossinemazzine@gmail.com>
2025-12-16 09:38:35 +0100

    Delete react-todo directory

--- react-todo/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  moduleFileExtensions: ["js", "jsx"],
-  transform: {
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
-};
-
